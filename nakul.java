dc81503f18e868cd896331b231f20ef71c8dcd3c
Vaibhavi-Mangrulkar <vaibhavimangrulkar00@gmail.com>
2025-01-06 19:13:35 +0530

    first

---
+++ nakul.java
@@ -0,0 +1,19 @@
+// Java Program to demonstrate
+// Default Constructor
+//import java.io.*;
+
+// Driver class
+class GFG {
+
+    // Default Constructor
+    //GFG() 
+    { 
+        System.out.println("Default constructor xyz"); 
+    }
+
+    // Driver function
+    public static void main(String[] args)
+    {
+        GFG hello = new GFG();
+    }
+}
